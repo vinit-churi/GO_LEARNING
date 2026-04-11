b26c685445ab0171c6fa8c58fa275d09f573ab7b
vinit-churi <vinitchuri@hotmail.com>
2026-04-11 12:10:25 +0530

    feat: learn go basic 01/01

--- lessons/01-beginner/01-setup-and-first-program/02-go-modules/starter.go
+++ lessons/01-beginner/01-setup-and-first-program/02-go-modules/starter.go
@@ -25,24 +25,34 @@ func main() {
 // Exercise 1: Return the module path
 func GetModulePath() string {
 	// TODO: Return a valid module path (e.g., "github.com/username/project")
-	return ""
+	return "github.com/vinit-churi/GO_LEARNING"
 }
 
 // Exercise 2: Return package information
 func GetPackageInfo() string {
 	// TODO: Return formatted package information
 	// Should include package name and purpose
-	return ""
+	return fmt.Sprintf("%s, %v", "github.com/vinit-churi/GO_LEARNING/lessongs/01-beginner/02-go-modules/starter", 0.1.0)
 }
 
 // Exercise 3: Return the module version
 func GetModuleVersion() string {
 	// TODO: Return a semantic version string (e.g., "v1.0.0")
-	return ""
+	return fmt.Sprintf("v%d.%d.%d", 1, 0, 0)
 }
 
 // Exercise 4: Print a complete module report
 func PrintModuleReport() {
 	// TODO: Print a formatted report combining all the above information
 	// Use the functions above to get the data
+
+	fmt.Printf("%s\n%s\n%s\n%s\n%s\n%s\n",
+	"=== Module Information ==="
+	"Module Path: github.com/yourname/go-learning"
+	"Package: main"
+	"Version: v1.0.0"
+	"Purpose: Learning Go modules and packages"
+	"========================"
+	)
+
 }
